f8b06ca5bb8ef7b5584642bb880f2d3b8d46a158
NOAH <noamom00@naver.com>
2025-11-04 18:16:35 +0900

    1104/pythonclass

--- day1103/01_LSTM_inco_deco.py
+++ day1103/01_LSTM_inco_deco.py
@@ -13,6 +13,7 @@ MAX_LEN = 60        # 문장 최대 길이 (문자 개수)
 
 SOS_TOKEN = "<" # start of sequence (decoder 시작)
 EOS_TOKEN = ">" # end of sequence   (decoder 종료)
+UNK_TOKEN = "U"
 PAD_ID = 0          # 패딩 인덱스
 
 DATA_PATH = "polite_pairs.txt"
@@ -47,16 +48,26 @@ print("예시:", src_texts[0], "->", tgt_texts[0])
 input_chars = set()
 target_chars = set()
 
+# for s in src_texts:
+#     for ch in s:
+#         input_chars.add(ch)
+
+# for t in tgt_texts:
+#     for ch in t:
+#         target_chars.add(ch)
+
 for s in src_texts:
-    for ch in s:
-        input_chars.add(ch)
+    for w in s.split():
+        input_chars.add(w)
 
 for t in tgt_texts:
-    for ch in t:
-        target_chars.add(ch)
+    for w in t.split():
+        target_chars.add(w)
 
 target_chars.add(SOS_TOKEN)
 target_chars.add(EOS_TOKEN)
+input_chars.add(UNK_TOKEN)
+target_chars.add(UNK_TOKEN)
 
 # 정렬해서 인덱스 고정
 input_chars = sorted(list(input_chars))
@@ -89,12 +100,28 @@ print('num_decoder_tokens:', num_decoder_tokens)
 #=================================
 
 # 문장을 숫자로 (정수 레이블링)
+# def text_to_ids(text, char2idx, add_sos=False, add_eos=False):
+#     ids = []
+#     if add_sos:
+#         ids.append(char2idx[SOS_TOKEN])
+#     for ch in text:
+#         ids.append(char2idx[ch])
+#     if add_eos:
+#         ids.append(char2idx[EOS_TOKEN])
+#     # 패딩
+#     if len(ids) < MAX_LEN:
+#         ids = ids + [PAD_ID] * (MAX_LEN - len(ids))
+#     else:
+#         ids = ids[:MAX_LEN]
+#     return ids
+
 def text_to_ids(text, char2idx, add_sos=False, add_eos=False):
     ids = []
     if add_sos:
         ids.append(char2idx[SOS_TOKEN])
-    for ch in text:
-        ids.append(char2idx[ch])
+    for w in text.split():
+        if w in char2idx:
+            ids.append(char2idx[w])
     if add_eos:
         ids.append(char2idx[EOS_TOKEN])
     # 패딩
@@ -199,3 +226,111 @@ train_model.fit(
     epochs=EPOCHS,
     verbose=1
 )
+
+#=================================
+# 5. 추론용 모델 분리
+#=================================
+
+# 인코더 추론 모델
+encoder_model = Model(encoder_inputs, encoder_states)
+
+# 디코더 추론 입력들
+dec_state_input_h = layers.Input(shape=(LATENT_DIM,), name="dec_state_h_in")
+dec_state_input_c = layers.Input(shape=(LATENT_DIM,), name="dec_state_c_in")
+dec_single_input = layers.Input(shape=(1,), name="dec_single_token")
+
+# 학습된 레이어 재사용 ()
+dec_emb_layer = train_model.get_layer("dec_embedding")
+dec_lstm_layer = train_model.get_layer("decoder_lstm")
+dec_dense_layer = train_model.get_layer("decoder_dense")
+
+dec_single_emb = dec_emb_layer(dec_single_input)
+dec_outputs_step, h_new, c_new = dec_lstm_layer(
+    dec_single_emb,
+    initial_state=[dec_state_input_h, dec_state_input_c]
+)
+dec_probs = dec_dense_layer(dec_outputs_step)
+
+decoder_model = Model(
+    [dec_single_input, dec_state_input_h, dec_state_input_c],
+    [dec_probs, h_new, c_new]
+)
+
+#=================================
+# 6. 디코딩 함수
+#=================================
+
+sos_id = target_char2idx[SOS_TOKEN]
+eos_id = target_char2idx[EOS_TOKEN]
+
+print('\n<SOS>:', sos_id, ' ', '<EOS>:', eos_id)
+
+# 정수화 및 패딩
+def encode_src(sentence):
+    # ids = []
+    # for ch in sentence:
+    #     if ch in input_char2idx:
+    #         ids.append(input_char2idx[ch])
+    #     else:
+    #         ids.append(input_char2idx["U"])
+    ids = [input_char2idx[w] for w in sentence.split() if w in input_char2idx]
+
+    if len(ids) < MAX_LEN:
+        ids = ids + [PAD_ID]*(MAX_LEN-len(ids))
+    else:
+        ids = ids[:MAX_LEN]
+    return np.array([ids], dtype=np.int32)
+
+def greedy_decode(input_sentence, max_len=60):
+    # 인코더로 state(h,c) 추출
+    enc_arr = encode_src(input_sentence)
+    h, c = encoder_model.predict(enc_arr, verbose=0)
+
+    # 디코더 시작 토큰 sos_id
+    target_seq = np.array([[sos_id]], dtype=np.int32)
+
+    decoded_ids = []
+
+    for _ in range(max_len):
+        probs, h, c = decoder_model.predict([target_seq, h, c], verbose=0)
+        token_id = np.argmax(probs[0, -1, :])
+
+        if token_id == eos_id:
+            break
+
+        decoded_ids.append(token_id)
+        target_seq = np.array([[token_id]], dtype=np.int32)
+
+    out_chars = []
+    for tid in decoded_ids:
+        if tid == 0: # 패딩은 무시해라
+            continue
+        if tid == input_char2idx["U"]:
+            continue
+        ch = target_idx2char.get(tid, "")
+        out_chars.append(ch)
+
+    return " ".join(out_chars)
+
+
+
+#=================================
+# 7. 테스트
+#=================================
+
+test_sentences = [
+    "지금 뭐 하고 계세요?",
+    "몸은 좀 괜찮으세요?",
+    "늦어서 죄송합니다.",
+    "걱정하지 마세요.",
+    "내일 시간 되세요?",
+    "천천히 오셔도 됩니다.",
+    "청주 날씨는 어때요?",
+    "도와주셔서 감사합니다.",
+    "오늘 기분은 어떠세요?"
+]
+
+for s in test_sentences:
+    print("INPUT :", s)
+    print("OUTPUT :", greedy_decode(s))
+    print("-----")
